7d9549d46692958e39b995c6ceb9e04f5a63de40
Elizabeth Rowe <lizrowe@email.unc.edu>
2021-08-31 13:58:45 -0400

    Progress on Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,7 +1,7 @@
 """Part 1 of exercise 1."""
 __author__ = "730403539"
 name: str = input("What is your name? ")
-print("You entered: ", name)
+print("You entered: ")
 hype_statement_1: str = input(name + ", you rock!")
 hype_statement_2: str = input("You're gonna kill this semester, " + name + "!")
 hype_statement_3: str = input("Don't be afraid, " + name + ", you got this!")
