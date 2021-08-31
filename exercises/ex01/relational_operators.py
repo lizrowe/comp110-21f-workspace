c8cb3382b3343209efabe265ea7ee5045298c30b
Elizabeth Rowe <lizrowe@email.unc.edu>
2021-08-31 14:03:06 -0400

    Progress on Exercise 1

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -2,7 +2,7 @@
 __author__ = "730403539"
 left_hand_number: str = input("Left-hand side: ")
 right_hand_number: str = input("Right-hand side: ")
-print(left_hand_number, " < ", right_hand_number, " is ", (left_hand_number < right_hand_number))
-print(left_hand_number, " >= ", right_hand_number, " is ", (left_hand_number >= right_hand_number))
-print(left_hand_number, " == ", right_hand_number, " is ", (left_hand_number == right_hand_number))
-print(left_hand_number, " != ", right_hand_number, " is ", (left_hand_number != right_hand_number))
+print(left_hand_number, "<", right_hand_number, "is", (left_hand_number < right_hand_number))
+print(left_hand_number, ">=", right_hand_number, "is", (left_hand_number >= right_hand_number))
+print(left_hand_number, "==", right_hand_number, "is", (left_hand_number == right_hand_number))
+print(left_hand_number, "!=", right_hand_number, "is", (left_hand_number != right_hand_number))
