480e45a4ecf2a4308791486dfd1a00fe80266153
Elizabeth Rowe <lizrowe@email.unc.edu>
2021-09-29 14:06:48 -0400

    Progress on Exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,31 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730403539"
 
 
 # TODO: Implement your functions here.
+
+
+def all(number_list: list[int], number: int) -> bool:
+    """Return True if the number is in the number list, False otherwise."""
+    i: int = 0
+    while i < len(number_list):
+        if number_list[i] != number:
+            return False
+        i += 1
+    return True
+
+
+def is_equal(number_list_one: list[int], number_list_two: list[int]) -> bool:
+    """Return True if lists are equal, False otherwise."""
+    i: int = 0
+    while i < len(number_list_one):
+        if number_list_one[i] != number_list_two[i]:
+            return False
+        i += 1
+    return True  
+
+
+def max(input: list[int]) -> int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
