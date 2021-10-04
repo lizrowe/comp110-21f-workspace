1c774a7493478d94de4164046beb0f10202b8673
Elizabeth Rowe <lizrowe@email.unc.edu>
2021-10-04 16:01:56 -0400

    Progress on Exercise 5

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -9,6 +9,8 @@ __author__ = "730403539"
 def all(number_list: list[int], number: int) -> bool:
     """Return True if the number is in the number list, False otherwise."""
     i: int = 0
+    if len(number_list) == 0:
+        return False
     while i < len(number_list):
         if number_list[i] != number:
             return False
@@ -19,6 +21,8 @@ def all(number_list: list[int], number: int) -> bool:
 def is_equal(number_list_one: list[int], number_list_two: list[int]) -> bool:
     """Return True if lists are equal, False otherwise."""
     i: int = 0
+    if len(number_list_one) != len(number_list_two):
+        return False
     while i < len(number_list_one):
         if number_list_one[i] != number_list_two[i]:
             return False
@@ -27,5 +31,8 @@ def is_equal(number_list_one: list[int], number_list_two: list[int]) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Return the largest value in the List."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
+    else:
+        return largest 
--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,17 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730403539"
 
 # Define your functions below
+
+
+def only_evens(number_list: list[int]) -> list[int]:
+    """Return only even numbers from the list."""
+    i: int = 0
+    o: list = []
+    if len(number_list) == 0:
+        return o
+    while i < len(number_list):
+        if i % 2 == 0:
+            return i
+        i += 1
