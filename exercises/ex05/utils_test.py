97a366838b0fae3eadb7b05bcff81c2d36883165
Elizabeth Rowe <lizrowe@email.unc.edu>
2021-10-05 18:02:04 -0400

    Progress on Exercise 5

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -8,21 +8,25 @@ __author__ = "730403539"
 
 
 def test_only_evens_empty() -> None:
+    """Tests only_evens function when empty."""
     number_list: list[int] = []
     assert only_evens(number_list) == []
 
 
 def test_only_evens_single_item() -> None:
+    """Tests only_evens function when there is a single item."""
     number_list: list[int] = [2]
     assert only_evens(number_list) == [2]
 
 
 def test_only_evens_many_items() -> None:
+    """Tests only_evens function when there are many items."""
     number_list: list[int] = [1, 2, 3, 4]
     assert only_evens(number_list) == [2, 4]
 
 
 def test_sub_empty() -> None:
+    """Tests sub function when empty."""
     list_1: list[int] = []
     start: int = 0 
     end: int = 0
@@ -30,6 +34,7 @@ def test_sub_empty() -> None:
 
 
 def test_sub_single_item() -> None:
+    """Tests sub function when there is a single item."""
     list_1: list[int] = [1]
     start: int = 1
     end: int = 1
@@ -37,6 +42,7 @@ def test_sub_single_item() -> None:
 
 
 def test_sub_many_items() -> None:
+    """Tests sub function when there are many items."""
     list_1: list[int] = [1, 2, 3, 4, 5]
     start: int = 1
     end: int = 4
@@ -44,18 +50,21 @@ def test_sub_many_items() -> None:
 
 
 def test_concat_empty() -> None:
+    """Tests concat function when empty."""
     list_1: list[int] = []
     list_2: list[int] = []
     assert concat(list_1, list_2) == []
 
 
 def test_concat_single_item() -> None:
+    """Tests concat function when there is a single item."""
     list_1: list[int] = [1]
     list_2: list[int] = [2]
     assert concat(list_1, list_2) == [1, 2]
 
 
 def test_concat_many_items() -> None:
+    """Tests concat function when there are many items."""
     list_1: list[int] = [1, 2, 3]
     list_2: list[int] = [4, 5, 6]
     assert concat(list_1, list_2) == [1, 2, 3, 4, 5, 6]
